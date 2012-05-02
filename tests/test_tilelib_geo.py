3a89dc55aac7cda8bd8c468588f7710e5879645a
Kotaimen.Cao <kotaimen.c@gmail.com>
2012-05-02 14:42:41 +0800

    change CRS value to string instead of SRID integers

--- tests/test_tilelib_geo.py
+++ tests/test_tilelib_geo.py
@@ -16,19 +16,68 @@ class TestCoordinate(unittest.TestCase):
     def tearDown(self):
         pass
 
-    def testInit(self):
+    def testInitDefault(self):
         coord = Coordinate()
         self.assertEqual(coord.longitude, 0)
         self.assertEqual(coord.latitude, 0)
         self.assertEqual(coord.lon, 0)
         self.assertEqual(coord.lat, 0)
-        self.assertEqual(coord.crs, 4326)
+        self.assertEqual(coord.crs, 'ESPG:4326')
+
+    def testInit(self):
+        coord = Coordinate(1.0, 2.0, 'ESPG:900913')
+        self.assertEqual(coord.longitude, 1.0)
+        self.assertEqual(coord.latitude, 2.0)
+        self.assertEqual(coord.lon, 1.0)
+        self.assertEqual(coord.lat, 2.0)
+        self.assertEqual(coord.crs, 'ESPG:900913')
 
     def testMakeTuple(self):
         coord = Coordinate(1, 2)
         self.assertEqual(coord.longitude, 1)
         self.assertEqual(coord.latitude, 2)
         self.assertEqual(coord.make_tuple(), (1, 2))
+
+    def testFromTuple(self):
+        coord = Coordinate.from_tuple((1, 2))
+        self.assertEqual(coord, Coordinate(1, 2))
+
+    def testRepr(self):
+        coord = Coordinate(1, 2)
+        self.assertEqual(repr(coord), 'Coordinate(1, 2)')
+
+
+class TestEnvelope(unittest.TestCase):
+
+    def testInit(self):
+        envelope = Envelope(0, 1, 2, 3)
+        self.assertEqual(envelope.left, 0)
+        self.assertEqual(envelope.bottom, 1)
+        self.assertEqual(envelope.right, 2)
+        self.assertEqual(envelope.top, 3)
+        self.assertEqual(envelope.lefttop, Coordinate(0, 3))
+        self.assertEqual(envelope.righttop, Coordinate(2, 3))
+        self.assertEqual(envelope.leftbottom, Coordinate(0, 1))
+        self.assertEqual(envelope.rightbottom, Coordinate(2, 1))
+        self.assertEqual(envelope.crs, 'ESPG:4326')
+
+    def testContains(self):
+        envelope = Envelope(0, 1, 2, 3)
+        self.assertTrue(envelope.contains(Coordinate(0.5, 1.5)))
+        self.assertTrue(envelope.contains(Coordinate(0, 1)))
+        self.assertTrue(envelope.contains(Coordinate(2, 3)))
+        self.assertFalse(envelope.contains(Coordinate(3, 3)))
+
+    def testIntersects(self):
+        envelope = Envelope(0, 1, 2, 3)
+        self.assertTrue(envelope.intersects(Envelope(0, 1, 2, 3)))
+        self.assertTrue(envelope.intersects(Envelope(2, 3, 4, 5)))
+        self.assertFalse(envelope.intersects(Envelope(4, 5, 6, 7)))
+        self.assertTrue(envelope.intersects(Envelope(0.5, 1.5, 3, 4)))
+
+    def testFromTuple(self):
+        envelope = Envelope.from_tuple((0, 1, 2, 3))
+        self.assertEqual(envelope, Envelope(0, 1, 2, 3))
 
 
 class TestProjection(unittest.TestCase):
