6497cd251385f2a7bc2cd78d57bf5ef524dfbd69
Kotaimen.Cao <kotaimen.c@gmail.com>
2012-05-03 22:36:08 +0800

    Add TileStorage IF

---
+++ mason/tilestorage/tilestorage.py
@@ -0,0 +1,94 @@
+'''
+Created on May 2, 2012
+
+@author: Kotaimen
+'''
+
+from ..tilelib import Tile  # unused
+
+class TileStorage(object):
+
+    """ Store Tile in persistence storage or a cache backend """
+
+    def __init__(self,
+                 tag='tilestorage',
+                 ):
+        self._tag = tag
+
+    @property
+    def tag(self):
+        return self._tag
+
+    def put(self, tile):
+        """ Put given Tile into cache, overwrite existing one if necessary
+
+        When put failed, implementation may choose to raise a
+        exception, or just pass silently.
+        This behavior is allowed because write operation may
+        be asynchronous.
+        """
+        raise NotImplementedError
+
+    def get(self, tile_index):
+        """ Get Tile from given TileIndex
+
+        Return a new Tile object, or None if Tile does not exist in the storage.
+        Note given TileIndex may be used to construct new Tile object.
+        """
+        raise NotImplementedError
+
+    def has(self, tile_index):
+        """ Check whether given TileIndex exists in the cache
+
+        Note implementation may provide a better way instead of actually
+        retrieve tile data.
+        """
+        return self.get(tile_index) is not None
+
+    def delete(self, tile_index):
+        """ Remove Tile with given TileIndex form storage
+
+        If Tile does not present in storage, this operation has no effect
+        """
+        raise NotImplementedError
+
+    def put_many(self, tiles):
+        """ Put many tiles into cache in one call
+
+        Note the default implement is *not* atomic. Implementation may
+        provide a more efficient way to do this.
+
+        For databases, this is usually done by using "executemany" so
+        its faster as well as being atomic.
+        """
+        for tile in tiles:
+            self.put(tile)
+
+    def get_multi(self, tile_indexes):
+        """ Get many tile in one call
+
+        Returns found Tiles in a dict which key is TileIndex.
+        If no Tile are not found in the storage, returns empty dict.
+        """
+        all_tiles = ((tile_index, self.get(tile_index)) for tile_index in tile_indexes)
+        # Discard those tile is None and return a dict
+        return dict((index, tile) for index, tile in all_tiles if tile is not None)
+
+    def delete_multi(self, tile_indexes):
+        """ Delete given Tiles in one call """
+        for tile_index in tile_indexes:
+            self.delete(tile_index)
+
+    def has_all(self, tile_indexes):
+        """ Check whether storage has all given tiles """
+        return all(self.has(tile_index) for tile_index in tile_indexes)
+
+    def has_any(self, tile_indexes):
+        """ Check whether storage has any given tiles """
+        return all(self.has(tile_index) for tile_index in tile_indexes)
+
+    def flush_all(self):
+        """ Delete all tiles in the storage """
+        raise NotImplementedError
+
+
