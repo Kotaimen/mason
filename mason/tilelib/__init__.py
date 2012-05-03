53a9e5c73941f04c73165d0a22772dae1937af0a
Kotaimen.Cao <kotaimen.c@gmail.com>
2012-05-03 20:22:11 +0800

    Calculate data hashing ind Tile CTOR

--- mason/tilelib/__init__.py
+++ mason/tilelib/__init__.py
@@ -1,4 +1,6 @@
 
+""" Mason core objects """
+
 from .geo import Coordinate, Point, Envelope, create_projection
 from .tile import Tile, TileIndex
-from pyramid import Pyramid
+from .pyramid import Pyramid
