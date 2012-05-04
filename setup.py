8521fe08f07d64501614fd4a8fea07ae4d4bb3be
Kotaimen <kotaimen.c@gmail.com>
2012-05-04 14:18:08 +0800

    Add skeleton to setup.py

--- setup.py
+++ setup.py
@@ -3,3 +3,21 @@ Created on Apr 29, 2012
 
 @author: Kotaimen
 '''
+
+from distutils.core import setup
+
+packages = []
+data_files = []
+scripts = []
+
+setup(name='mason',
+    version='0.8.0',
+    author='Kotaimen, Ray',
+    author_email='kotaimen.c@gmail.com, gliese.q@gmail.com',
+    description='Another map tile library reinvented',
+    packages=packages,
+    data_files=data_files,
+    scripts=scripts,
+)
+
+
