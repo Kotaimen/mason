cbf605cc26320432929db44b2ef5958e0ff59567
ray <gliese.q@gmail.com>
2012-09-18 14:43:08 +0800

    fix test for datasource renderer

--- tests/test_renderer_datasource.py
+++ tests/test_renderer_datasource.py
@@ -5,7 +5,8 @@ Created on Sep 10, 2012
 '''
 import unittest
 from mason.core import Pyramid
-from mason.renderer import MetaTileDataSourceFactory
+from mason.cartographer import CartographerFactory
+from mason.renderer.datasource import CartographerMetaTileDataSource
 
 
 class MetaTileDataSourceTest(unittest.TestCase):
@@ -14,10 +15,11 @@ class MetaTileDataSourceTest(unittest.TestCase):
         pyramid = Pyramid(tile_size=512)
         metatile_index = pyramid.create_metatile_index(3, 2, 2, 2)
 
-        prototype = MetaTileDataSourceFactory.CARTO_MAPNIK
-        parameters = dict(theme='./input/world.xml', image_type='png')
-        datasource = MetaTileDataSourceFactory(prototype, **parameters)
-        metatile = datasource.get(metatile_index)
+        params = dict(theme='./input/world.xml', image_type='png')
+        carto = CartographerFactory('mapnik', **params)
+        metatile_datasource = CartographerMetaTileDataSource(carto)
+
+        metatile = metatile_datasource.get(metatile_index)
 
         self.assertIsNotNone(metatile)
         self.assertIsNotNone(metatile.data)
